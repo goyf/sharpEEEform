333a79af6609a2815f05cd87e88393e8431b18e6
Jan <9010650+goyf@users.noreply.github.com>
2019-02-16 18:25:45 +0000

    first upload

---
+++ gui test/gui test/Form1.Designer.cs
@@ -0,0 +1,383 @@
+﻿namespace gui_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.whereBox = new System.Windows.Forms.TextBox();
+            this.whenBox = new System.Windows.Forms.TextBox();
+            this.frankieBox = new System.Windows.Forms.CheckBox();
+            this.jackBox = new System.Windows.Forms.CheckBox();
+            this.janBox = new System.Windows.Forms.CheckBox();
+            this.nikBox = new System.Windows.Forms.CheckBox();
+            this.tomBox = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.prepBox = new System.Windows.Forms.TextBox();
+            this.exeBox = new System.Windows.Forms.TextBox();
+            this.recBox = new System.Windows.Forms.TextBox();
+            this.conBox = new System.Windows.Forms.TextBox();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.attBox = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.nameBox, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.whereBox, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.whenBox, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.frankieBox, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.jackBox, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.janBox, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.nikBox, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.tomBox, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 6);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 0, 7);
+            this.tableLayoutPanel1.Controls.Add(this.label6, 0, 8);
+            this.tableLayoutPanel1.Controls.Add(this.label7, 0, 9);
+            this.tableLayoutPanel1.Controls.Add(this.prepBox, 1, 6);
+            this.tableLayoutPanel1.Controls.Add(this.exeBox, 1, 7);
+            this.tableLayoutPanel1.Controls.Add(this.recBox, 1, 8);
+            this.tableLayoutPanel1.Controls.Add(this.conBox, 1, 9);
+            this.tableLayoutPanel1.Controls.Add(this.submitButton, 1, 10);
+            this.tableLayoutPanel1.Controls.Add(this.label8, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.attBox, 1, 5);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 11;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.331666F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.331666F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.331666F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.331666F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.331666F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.331666F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.002F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.002F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.002F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.002F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.002F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 662);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(260, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name of the event";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(3, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(260, 55);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Where";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Location = new System.Drawing.Point(3, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(260, 55);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "When";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // nameBox
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.nameBox, 2);
+            this.nameBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.nameBox.Location = new System.Drawing.Point(269, 3);
+            this.nameBox.Multiline = true;
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(528, 49);
+            this.nameBox.TabIndex = 3;
+            // 
+            // whereBox
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.whereBox, 2);
+            this.whereBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.whereBox.Location = new System.Drawing.Point(269, 58);
+            this.whereBox.Multiline = true;
+            this.whereBox.Name = "whereBox";
+            this.whereBox.Size = new System.Drawing.Size(528, 49);
+            this.whereBox.TabIndex = 4;
+            // 
+            // whenBox
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.whenBox, 2);
+            this.whenBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.whenBox.Location = new System.Drawing.Point(269, 113);
+            this.whenBox.Multiline = true;
+            this.whenBox.Name = "whenBox";
+            this.whenBox.Size = new System.Drawing.Size(528, 49);
+            this.whenBox.TabIndex = 5;
+            // 
+            // frankieBox
+            // 
+            this.frankieBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.frankieBox.AutoSize = true;
+            this.frankieBox.Location = new System.Drawing.Point(75, 178);
+            this.frankieBox.Name = "frankieBox";
+            this.frankieBox.Size = new System.Drawing.Size(116, 29);
+            this.frankieBox.TabIndex = 6;
+            this.frankieBox.Text = "Frankie";
+            this.frankieBox.UseVisualStyleBackColor = true;
+            this.frankieBox.CheckedChanged += new System.EventHandler(this.frankieBox_CheckedChanged);
+            // 
+            // jackBox
+            // 
+            this.jackBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.jackBox.AutoSize = true;
+            this.jackBox.Location = new System.Drawing.Point(354, 178);
+            this.jackBox.Name = "jackBox";
+            this.jackBox.Size = new System.Drawing.Size(89, 29);
+            this.jackBox.TabIndex = 7;
+            this.jackBox.Text = "Jack";
+            this.jackBox.UseVisualStyleBackColor = true;
+            this.jackBox.CheckedChanged += new System.EventHandler(this.jackBox_CheckedChanged);
+            // 
+            // janBox
+            // 
+            this.janBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.janBox.AutoSize = true;
+            this.janBox.Location = new System.Drawing.Point(626, 178);
+            this.janBox.Name = "janBox";
+            this.janBox.Size = new System.Drawing.Size(79, 29);
+            this.janBox.TabIndex = 8;
+            this.janBox.Text = "Jan";
+            this.janBox.UseVisualStyleBackColor = true;
+            this.janBox.CheckedChanged += new System.EventHandler(this.janBox_CheckedChanged);
+            // 
+            // nikBox
+            // 
+            this.nikBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.nikBox.AutoSize = true;
+            this.nikBox.Location = new System.Drawing.Point(95, 233);
+            this.nikBox.Name = "nikBox";
+            this.nikBox.Size = new System.Drawing.Size(75, 29);
+            this.nikBox.TabIndex = 9;
+            this.nikBox.Text = "Nik";
+            this.nikBox.UseVisualStyleBackColor = true;
+            this.nikBox.CheckedChanged += new System.EventHandler(this.nikBox_CheckedChanged);
+            // 
+            // tomBox
+            // 
+            this.tomBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tomBox.AutoSize = true;
+            this.tomBox.Location = new System.Drawing.Point(356, 233);
+            this.tomBox.Name = "tomBox";
+            this.tomBox.Size = new System.Drawing.Size(86, 29);
+            this.tomBox.TabIndex = 10;
+            this.tomBox.Text = "Tom";
+            this.tomBox.UseVisualStyleBackColor = true;
+            this.tomBox.CheckedChanged += new System.EventHandler(this.tomBox_CheckedChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Location = new System.Drawing.Point(3, 330);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(260, 66);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Preparation";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label5.Location = new System.Drawing.Point(3, 396);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(260, 66);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Execution";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label6.Location = new System.Drawing.Point(3, 462);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(260, 66);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Recommendation";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label7.Location = new System.Drawing.Point(3, 528);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(260, 66);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Conclusion";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // prepBox
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.prepBox, 2);
+            this.prepBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.prepBox.Location = new System.Drawing.Point(269, 333);
+            this.prepBox.Multiline = true;
+            this.prepBox.Name = "prepBox";
+            this.prepBox.Size = new System.Drawing.Size(528, 60);
+            this.prepBox.TabIndex = 15;
+            // 
+            // exeBox
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.exeBox, 2);
+            this.exeBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.exeBox.Location = new System.Drawing.Point(269, 399);
+            this.exeBox.Multiline = true;
+            this.exeBox.Name = "exeBox";
+            this.exeBox.Size = new System.Drawing.Size(528, 60);
+            this.exeBox.TabIndex = 16;
+            // 
+            // recBox
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.recBox, 2);
+            this.recBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.recBox.Location = new System.Drawing.Point(269, 465);
+            this.recBox.Multiline = true;
+            this.recBox.Name = "recBox";
+            this.recBox.Size = new System.Drawing.Size(528, 60);
+            this.recBox.TabIndex = 17;
+            // 
+            // conBox
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.conBox, 2);
+            this.conBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.conBox.Location = new System.Drawing.Point(269, 531);
+            this.conBox.Multiline = true;
+            this.conBox.Name = "conBox";
+            this.conBox.Size = new System.Drawing.Size(528, 60);
+            this.conBox.TabIndex = 18;
+            // 
+            // submitButton
+            // 
+            this.submitButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.submitButton.Location = new System.Drawing.Point(269, 597);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(260, 62);
+            this.submitButton.TabIndex = 19;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label8.Location = new System.Drawing.Point(3, 275);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(260, 55);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "Attendance";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // attBox
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.attBox, 2);
+            this.attBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.attBox.Location = new System.Drawing.Point(269, 278);
+            this.attBox.Multiline = true;
+            this.attBox.Name = "attBox";
+            this.attBox.Size = new System.Drawing.Size(528, 49);
+            this.attBox.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 662);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.TextBox whereBox;
+        private System.Windows.Forms.TextBox whenBox;
+        private System.Windows.Forms.CheckBox frankieBox;
+        private System.Windows.Forms.CheckBox jackBox;
+        private System.Windows.Forms.CheckBox janBox;
+        private System.Windows.Forms.CheckBox nikBox;
+        private System.Windows.Forms.CheckBox tomBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox prepBox;
+        private System.Windows.Forms.TextBox exeBox;
+        private System.Windows.Forms.TextBox recBox;
+        private System.Windows.Forms.TextBox conBox;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox attBox;
+    }
+}
+
---
+++ gui test/gui test/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+
+using System.IO; //input and output
+
+using iTextSharp;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace gui_test
+{
+    public partial class Form1 : Form
+    {
+        int[] com_mem = new int[5];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void frankieBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (frankieBox.Checked)
+                com_mem[0] = 1;
+        }
+
+        private void jackBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (jackBox.Checked)
+                com_mem[1] = 1;
+        }
+
+        private void janBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (janBox.Checked)
+                com_mem[2] = 1;
+        }
+
+        private void nikBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (nikBox.Checked)
+                com_mem[3] = 1;
+        }
+
+        private void tomBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (tomBox.Checked)
+                com_mem[4] = 1;
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+
+            string nameText = nameBox.Text;
+            string whereText = whereBox.Text;
+            string whenText = whenBox.Text;
+
+            string memText = "";
+
+            if (com_mem[0] == 1)
+                memText = memText + frankieBox.Text + ',';
+            if (com_mem[1] == 1)
+                memText = memText + jackBox.Text + ',';
+            if (com_mem[2] == 1)
+                memText = memText + janBox.Text + ',';
+            if (com_mem[3] == 1)
+                memText = memText + nikBox.Text + ',';
+            if (com_mem[4] == 1)
+                memText = memText + tomBox.Text;
+
+            string attText = attBox.Text;
+
+            string prepText = prepBox.Text;
+
+            string exeText = exeBox.Text;
+
+            string recText = recBox.Text;
+
+            string conText = conBox.Text;
+
+            
+            string[] passString = { nameText, whereText, whenText, memText, attText, prepText, exeText, recText, conText };
+            pdfTestSharp(passString);
+
+
+        }
+
+        enum field { what=0, where, when, who, att, prep, exe, rec, con };
+        private void pdfTestSharp(string[] passString)
+        {
+            //string path = @"D:\c_sharp_tests\";
+
+
+            string fileName = "eventReport" + passString[(int)field.when] + ".pdf";
+            //FileStream fs = new FileStream(path+fileName, FileMode.Create, FileAccess.Write, FileShare.None);
+            FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.None);
+
+            Document doc = new Document();
+            PdfWriter writer = PdfWriter.GetInstance(doc, fs);
+            doc.Open();
+            int cols = 3;
+            PdfPTable table = new PdfPTable(cols);
+            PdfPCell titleCell = new PdfPCell(new Phrase("Event report"));
+            titleCell.Colspan = cols;
+            titleCell.HorizontalAlignment = 1; //0=Left, 1=Centre, 2=Right
+            table.AddCell(titleCell);
+
+            PdfPCell whatCell = new PdfPCell(new Phrase("Name of the event"));
+            table.AddCell(whatCell);
+            PdfPCell whatEntryCell = new PdfPCell(new Phrase(passString[(int) field.what]));
+            whatEntryCell.Colspan = cols - 1;
+            table.AddCell(whatEntryCell);
+
+            PdfPCell whenCell = new PdfPCell(new Phrase("When"));
+            table.AddCell(whenCell);
+            PdfPCell whenEntryCell = new PdfPCell(new Phrase(passString[(int)field.when]));
+            whenEntryCell.Colspan = cols - 1;
+            table.AddCell(whenEntryCell);
+
+            PdfPCell whereCell = new PdfPCell(new Phrase("Where"));
+            table.AddCell(whereCell);
+            PdfPCell whereEntryCell = new PdfPCell(new Phrase(passString[(int)field.where]));
+            whereEntryCell.Colspan = cols - 1;
+            table.AddCell(whereEntryCell);
+
+            PdfPCell whoCell = new PdfPCell(new Phrase("Who"));
+            table.AddCell(whoCell);
+            PdfPCell whoEntryCell = new PdfPCell(new Phrase(passString[(int)field.who]));
+            whoEntryCell.Colspan = cols - 1;
+            table.AddCell(whoEntryCell);
+
+            PdfPCell attCell = new PdfPCell(new Phrase("Attendance"));
+            table.AddCell(attCell);
+            PdfPCell attEntryCell = new PdfPCell(new Phrase(passString[(int)field.att]));
+            attEntryCell.Colspan = cols - 1;
+            table.AddCell(attEntryCell);
+
+            PdfPCell prepCell = new PdfPCell(new Phrase("Preparation"));
+            table.AddCell(prepCell);
+            PdfPCell prepEntryCell = new PdfPCell(new Phrase(passString[(int)field.prep]));
+            prepEntryCell.Colspan = cols - 1;
+            table.AddCell(prepEntryCell);
+
+            PdfPCell exeCell = new PdfPCell(new Phrase("Execution"));
+            table.AddCell(exeCell);
+            PdfPCell exeEntryCell = new PdfPCell(new Phrase(passString[(int)field.exe]));
+            exeEntryCell.Colspan = cols - 1;
+            table.AddCell(exeEntryCell);
+
+            PdfPCell recCell = new PdfPCell(new Phrase("Recommendation"));
+            table.AddCell(recCell);
+            PdfPCell recEntryCell = new PdfPCell(new Phrase(passString[(int)field.rec]));
+            recEntryCell.Colspan = cols - 1;
+            table.AddCell(recEntryCell);
+
+            PdfPCell conCell = new PdfPCell(new Phrase("Conclusion"));
+            table.AddCell(conCell);
+            PdfPCell conEntryCell = new PdfPCell(new Phrase(passString[(int)field.con]));
+            conEntryCell.Colspan = cols - 1;
+            table.AddCell(conEntryCell);
+
+            doc.Add(table);
+            doc.Close();
+        }
+
+    }
+
+    
+
+}
+
